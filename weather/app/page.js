974a449bd8375947292bf45b180bb8a21b9abc94
Manish-Chatterjee <99247194+Manish-Chatterjee@users.noreply.github.com>
2024-05-09 21:34:28 +0530

    commited

--- weather/app/page.js
+++ weather/app/page.js
@@ -40,6 +40,21 @@ const page = () => {
   const sunsetformattedTime = sunsetTime.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hourCycle: 'h12' });
   const sunsetTimeDisplay = sunsetformattedTime === 'Invalid Date' ? '--:--' : sunsetformattedTime
 
+  const firstWeatherData = weather?.weather?.[0].main;
+  console.log(firstWeatherData,'data24')
+
+  const getWeatherScene = () => {
+    if (firstWeatherData === "Rain" || firstWeatherData === "Thunderstorm") {
+      return <Spline scene="https://prod.spline.design/aFEUjLWaT3f-AR5G/scene.splinecode" />;
+    } else if (firstWeatherData === "Clouds" || firstWeatherData === "Haze") {
+      return <Spline scene="https://prod.spline.design/CHCTlddJ5h1K3hfr/scene.splinecode" />;
+    } else if (firstWeatherData === "Clear") {
+      return <Spline scene="https://prod.spline.design/lcd1KzMAgj02Gz-s/scene.splinecode" />;
+    } else {
+      return null;
+    }
+  };
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -68,130 +83,146 @@ const page = () => {
 
 console.log(weather,'weather')
 
-const scenes = {
-  scene1: 'https://prod.spline.design/aFEUjLWaT3f-AR5G/scene.splinecode',
-  scene2: 'https://prod.spline.design/CHCTlddJ5h1K3hfr/scene.splinecode',
-}
+// const scenes = {
+//   scene1: 'https://prod.spline.design/aFEUjLWaT3f-AR5G/scene.splinecode',
+//   scene2: 'https://prod.spline.design/CHCTlddJ5h1K3hfr/scene.splinecode',
+// }
 
 // const data = {
 //   main: 'rain',
 // };
 
   return (
-    <div className='main'>
-      <Image src={bgImg} alt='bg' layout='fill' objectFit='cover' objectPosition='center' />
-      <div className='container'>
-        <input
-          type="text"
-          value={place}
-          onChange={(event) => setPlace(event.target.value)}
-          onKeyDown={handleKeyDown}  
-          placeholder="place name"
-          className='placeName'
-        />
-        <div className='locationInfo'>
-          <div id='dataLeft'>
-            {/* <p id='temp'>temp:{weather?.main?.temp}</p> */}
-            <span className='dataDisplay'>
+    <div>
+      <div className='main'>
+        <Image src={bgImg} alt='bg' layout='fill' objectFit='cover' objectPosition='center' />
+        <div className="message"><p>these app better runs on desktop</p></div>
+        <div className='container'>
+          <input
+            type="text"
+            value={place}
+            onChange={(event) => setPlace(event.target.value)}
+            onKeyDown={handleKeyDown}  
+            placeholder="place name"
+            className='placeName'
+          />
+          <div className='locationInfo'>
+            <div id='dataLeft'>
+              {/* <p id='temp'>temp:{weather?.main?.temp}</p> */}
+              <span className='dataDisplay'>
+                <p className='icons'></p>
+                <p className='iconsText'>humidity</p>
+                <p className='iconsNo'>{weather?.main?.humidity !== undefined ? weather?.main?.humidity +' %' : '--'}</p>
+              </span>
+              <span className='dataDisplay'>
               <p className='icons'></p>
-              <p className='iconsText'>humidity</p>
-              <p className='iconsNo'>{weather?.main?.humidity !== undefined ? weather?.main?.humidity +' %' : '--'}</p>
-            </span>
-            <span className='dataDisplay'>
-            <p className='icons'></p>
-              <p className='iconsText'>pressure</p>
-              <p className='iconsNo'>{weather?.main?.pressure !== undefined ? weather?.main?.pressure +' hPa' : '--'}</p>
-            </span>
-            <span className='dataDisplay'>
-            <p className='icons'></p>
-              <p className='iconsText'>visibility</p>
-              <p className='iconsNo'>{weather?.visibility !== undefined ? (weather?.visibility / 1000).toFixed(1) +' km' : '--'}</p>
-            </span>
-            <span className='dataDisplay'>
-            <p className='icons'></p>
-              <p className='iconsText'>clouds</p>
-              <p className='iconsNo'>{weather?.clouds?.all !== undefined ? weather?.clouds?.all +' %' : '--'}</p>
-            </span>
-            <span className='dataDisplay'>
+                <p className='iconsText'>pressure</p>
+                <p className='iconsNo'>{weather?.main?.pressure !== undefined ? weather?.main?.pressure +' hPa' : '--'}</p>
+              </span>
+              <span className='dataDisplay'>
               <p className='icons'></p>
-              <p className='iconsText'>sunrise</p>
-              {/* <p className='iconsNo'>{weather?.sys?.sunrise}</p> */}
-              {/* <p className='iconsNo'>{`${Math.floor(weather?.sys?.sunrise / 3600) % 24}:${Math.floor((weather?.sys?.sunrise / 60) % 60).toString().padStart(2, '0')}:${(weather?.sys?.sunrise % 60).toString().padStart(2, '0')}`}</p> */}
-              <p className='iconsNo'>{sunriseTimeDisplay}</p>
-            </span>
-          </div>
-          <div id='dataCenter'>
-            <div id='cloudyStats'>
-
-              {weather?.weather?.map((data) => {
-                  return(
-                    <div>
-                    {/* <p>main:{data.main}</p> */}
-                    <p>{data.description}</p>
-                    </div>
-                  )
-              })}
+                <p className='iconsText'>visibility</p>
+                <p className='iconsNo'>{weather?.visibility !== undefined ? (weather?.visibility / 1000).toFixed(1) +' km' : '--'}</p>
+              </span>
+              <span className='dataDisplay'>
+              <p className='icons'></p>
+                <p className='iconsText'>clouds</p>
+                <p className='iconsNo'>{weather?.clouds?.all !== undefined ? weather?.clouds?.all +' %' : '--'}</p>
+              </span>
+              <span className='dataDisplay'>
+                <p className='icons'></p>
+                <p className='iconsText'>sunrise</p>
+                {/* <p className='iconsNo'>{weather?.sys?.sunrise}</p> */}
+                {/* <p className='iconsNo'>{`${Math.floor(weather?.sys?.sunrise / 3600) % 24}:${Math.floor((weather?.sys?.sunrise / 60) % 60).toString().padStart(2, '0')}:${(weather?.sys?.sunrise % 60).toString().padStart(2, '0')}`}</p> */}
+                <p className='iconsNo'>{sunriseTimeDisplay}</p>
+              </span>
             </div>
-            <div className='spline'>
-            {/* <Spline scene="https://prod.spline.design/CHCTlddJ5h1K3hfr/scene.splinecode" /> */}
-            {/* <Spline scene="https://prod.spline.design/CHCTlddJ5h1K3hfr/scene.splinecode" /> */}
-            {/* Rainy */}
-              {/* // (<Spline scene="https://prod.spline.design/aFEUjLWaT3f-AR5G/scene.splinecode" />) */}
-            {weather?.weather?.map((data) => {
-              console.log(data.main,'data18')
-              // if (data.main === "Clouds") {
-              //   return <Spline scene="https://prod.spline.design/aFEUjLWaT3f-AR5G/scene.splinecode" />;
-              // } else {
-              //   return <Spline scene="https://prod.spline.design/CHCTlddJ5h1K3hfr/scene.splinecode" />;
-              // }
-              return(<Spline scene="https://prod.spline.design/CHCTlddJ5h1K3hfr/scene.splinecode" />)
-            })}
-            {/* <Spline scene="https://prod.spline.design/aFEUjLWaT3f-AR5G/scene.splinecode" /> */}
-            {/* <Spline scene="https://prod.spline.design/aFEUjLWaT3f-AR5G/scene.splinecode" /> */}
+            <div id='dataCenter'>
+              <div id='cloudyStats'>
+
+                {weather?.weather?.map((data) => {
+                    return(
+                      <div>
+                      {/* <p>main:{data.main}</p> */}
+                      <p>{data.description}</p>
+                      </div>
+                    )
+                })}
+              </div>
+              <div className='spline'>
+              {/* <Spline scene="https://prod.spline.design/CHCTlddJ5h1K3hfr/scene.splinecode" /> */}
+              {/* <Spline scene="https://prod.spline.design/CHCTlddJ5h1K3hfr/scene.splinecode" /> */}
+              {/* Rainy */}
+                {/* // (<Spline scene="https://prod.spline.design/aFEUjLWaT3f-AR5G/scene.splinecode" />) */}
+
+
+              {/* {weather?.weather?.map((data) => {
+                console.log(data.main,'data18')
+                if (data.main === "Rain" || "Thunderstorm") {
+                  return <Spline scene="https://prod.spline.design/aFEUjLWaT3f-AR5G/scene.splinecode" />;
+                } else if (data.main === "Clouds") {
+                  return <Spline scene="https://prod.spline.design/CHCTlddJ5h1K3hfr/scene.splinecode" />;
+                } else {
+                  return <Spline scene="https://prod.spline.design/lcd1KzMAgj02Gz-s/scene.splinecode" />;
+                }
+                // return(<Spline scene="https://prod.spline.design/CHCTlddJ5h1K3hfr/scene.splinecode" />)
+              })} */}
+
+
+                {/* {firstWeatherData === "Rain" || "Thunderstorm" ? <Spline scene="https://prod.spline.design/aFEUjLWaT3f-AR5G/scene.splinecode" /> :null}
+                {firstWeatherData === "Clouds" ? <Spline scene="https://prod.spline.design/CHCTlddJ5h1K3hfr/scene.splinecode" /> :null}
+                {firstWeatherData === "Clear" ? <Spline scene="https://prod.spline.design/lcd1KzMAgj02Gz-s/scene.splinecode" /> :null} */}
+
+                {getWeatherScene()}
+
+              {/* <Spline scene="https://prod.spline.design/aFEUjLWaT3f-AR5G/scene.splinecode" /> */}
+              {/* <Spline scene="https://prod.spline.design/aFEUjLWaT3f-AR5G/scene.splinecode" /> */}
+              </div>
+              {weather?.main?.temp &&
+              <p id='temp'>{Math.ceil(weather?.main?.temp)} ºC</p>}
             </div>
-            {weather?.main?.temp &&
-            <p id='temp'>{Math.ceil(weather?.main?.temp)} ºC</p>}
-          </div>
-          <div id='dataRight'>
-            <span className='dataDisplay'>
-              <p className='icons'></p>
-              <p className='iconsText'>wind Deg</p>
-              <p className='iconsNo'>{weather?.wind?.deg !== undefined ? weather?.wind?.deg +' °' : '--'}</p>
-            </span>
-            <span className='dataDisplay'>
-              <p className='icons'></p>
-              <p className='iconsText'>wind Speed</p>
-              <p className='iconsNo'>{weather?.wind?.speed !== undefined ? weather?.wind?.speed +' m/s' : '--'}</p>
-            </span>
-            <span className='dataDisplay'>
-              <p className='icons'></p>
-              <p className='iconsText'>wind Gust</p>
-              <p className='iconsNo'>{weather?.wind?.gust !== undefined ? weather?.wind?.gust +' m/s' : '--'}</p>
-            </span>
-            {/* {weather?.weather?.map((data) => {
-              return(
-                <div>
-                <p>main:{data.main}</p>
-                <p>description:{data.description}</p>
-                </div>
-              )
-            })} */}
+            <div id='dataRight'>
+              <span className='dataDisplay'>
+                <p className='icons'></p>
+                <p className='iconsText'>wind Deg</p>
+                <p className='iconsNo'>{weather?.wind?.deg !== undefined ? weather?.wind?.deg +' °' : '--'}</p>
+              </span>
+              <span className='dataDisplay'>
+                <p className='icons'></p>
+                <p className='iconsText'>wind Speed</p>
+                <p className='iconsNo'>{weather?.wind?.speed !== undefined ? weather?.wind?.speed +' m/s' : '--'}</p>
+              </span>
+              <span className='dataDisplay'>
+                <p className='icons'></p>
+                <p className='iconsText'>wind Gust</p>
+                <p className='iconsNo'>{weather?.wind?.gust !== undefined ? weather?.wind?.gust +' m/s' : '--'}</p>
+              </span>
+              {/* {weather?.weather?.map((data) => {
+                return(
+                  <div>
+                  <p>main:{data.main}</p>
+                  <p>description:{data.description}</p>
+                  </div>
+                )
+              })} */}
 
 
-            <span className='dataDisplay'>
-              <p className='icons'></p>
-              <p className='iconsText'>timezone</p>
-              <p className='iconsNo'>{timezoneCondition}</p>
-            </span>
-            <span className='dataDisplay'>
-              <p className='icons'></p>
-              <p className='iconsText'>sunset</p>
-              {/* <p className='iconsNo'>{weather?.sys?.sunset}</p> */}
-              {/* <p className='iconsNo'>{`${Math.floor(weather?.sys?.sunset / 3600) % 24}:${Math.floor((weather?.sys?.sunset / 60) % 60).toString().padStart(2, '0')}:${(weather?.sys?.sunset % 60).toString().padStart(2, '0')}`}</p> */}
-              <p className='iconsNo'>{sunsetTimeDisplay}</p>
-            </span>
+              <span className='dataDisplay'>
+                <p className='icons'></p>
+                <p className='iconsText'>timezone</p>
+                <p className='iconsNo'>{timezoneCondition}</p>
+              </span>
+              <span className='dataDisplay'>
+                <p className='icons'></p>
+                <p className='iconsText'>sunset</p>
+                {/* <p className='iconsNo'>{weather?.sys?.sunset}</p> */}
+                {/* <p className='iconsNo'>{`${Math.floor(weather?.sys?.sunset / 3600) % 24}:${Math.floor((weather?.sys?.sunset / 60) % 60).toString().padStart(2, '0')}:${(weather?.sys?.sunset % 60).toString().padStart(2, '0')}`}</p> */}
+                <p className='iconsNo'>{sunsetTimeDisplay}</p>
+              </span>
+            </div>
           </div>
         </div>
+        <div className='info'>sunrise and sunset timings are based on local time</div>
       </div>
     </div>
   )
